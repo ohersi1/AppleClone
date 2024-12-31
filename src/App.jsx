1fc74113349a3589e4d13eb8d0484ae6558f7015
Osman Hersi <osmanhersi@Osmans-MacBook-Air.local>
2024-12-31 18:30:19 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
