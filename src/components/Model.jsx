2183ed81c519b87b6b1227c9f0a4f1fd212d6994
Osman Hersi <osmanhersi@Osmans-MacBook-Air.local>
2025-01-14 01:20:23 +0000

    finished highlights section

---
+++ src/components/Model.jsx
@@ -0,0 +1,13 @@
+const Model = () => {
+  return (
+    <section>
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+            Take a closer look.
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default Model
