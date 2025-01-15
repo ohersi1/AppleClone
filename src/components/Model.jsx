47a9e0bd8f5ce46b429d5f0e54a5e8c79771907c
Osman Hersi <osmanhersi@hotmail.co.uk>
2025-01-15 22:38:22 +0000

    continued working on model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -73,6 +73,9 @@ const Model = () => {
               <View.Port />
             </Canvas>
           </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+          </div>
         </div>
       </div>
     </section>
