e069b9e0414e77b16eacf9fd21d1042a00e134a3
Osman Hersi <osmanhersi@hotmail.co.uk>
2025-01-20 20:14:53 +0000

    Finally got the iPhone 3d rendering working

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,4 +1,13 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import * as THREE from "three";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import Iphone from "./Iphone.jsx";
 
 const ModelView = ({
   index,
@@ -13,13 +22,36 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${
-        index === 2 ? "right-[-100%]" : ""
-      }`}
+      className={`w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading...</div>
+            </Html>
+          }
+        >
+          <Iphone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
+        </Suspense>
+      </group>
     </View>
   );
 };
